efef980e70854ba10764a6dd2bc13922d07b5426
weet <115834022+weet-ops@users.noreply.github.com>
2024-10-23 13:59:24 -0500

    the real deal code
    feel free to edit

---
+++ pokernight.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+
+print("Poker Night - A very accurate simulation of the casino")
+print("Base code by ImmuneSnow, modified by weet.")
+input("Press any key to start wasting your life away.")
+time.sleep(1)
+
+def coin_flip():
+    return 'H' if random.randint(0, 1) else 'T'
+
+def main():
+    chips = 200
+    money = 100
+    print("You currently have " + str(chips) + " chips.")
+    print(" ")
+    print("You currently have " + str(money) + " dollars in your wallet.")
+    while chips > 0:
+        try:
+            bet = int(input(f"You have {chips} chips. How many would you like to bet on the coin flip? "))
+        except ValueError:
+            print("Invalid amount. Please enter an integer.")
+            continue
+        if bet > chips:
+            print("You can't bet more than you have.")
+            continue
+        guess = input("Enter 'H' for heads or 'T' for tails: ").upper()
+        flip = coin_flip()
+        if guess == flip:
+            print("You won!" + str(bet) + " chips")
+            chips += bet
+        else:
+            print("You lost! -" + str(bet) + " chips")
+            chips -= bet
+        print(f"You now have {chips} chips.\n")
+    print("You ran out of chips.")
+
+if __name__ == "__main__":
+    main()
